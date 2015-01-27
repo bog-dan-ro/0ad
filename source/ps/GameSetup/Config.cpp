0429ebe20ee3c4ffccce108352cfd76366ff3f4c
BogDan Vatra <bogdan@kde.org>
2015-01-27 14:28:43 +0200

    Scale the GUI on Android.

--- source/ps/GameSetup/Config.cpp
+++ source/ps/GameSetup/Config.cpp
@@ -56,7 +56,12 @@ float g_Gamma = 1.0f;
 CStr g_RenderPath = "default";
 
 int g_xres, g_yres;
+#if OS_ANDROID
+#warning FIXME HACK, Andriod has enough info to compute this variable
+float g_GuiScale = 0.5f;
+#else
 float g_GuiScale = 1.0f;
+#endif
 bool g_VSync = false;
 
 bool g_Quickstart = false;
